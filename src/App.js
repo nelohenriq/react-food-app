7a3f7164c2b2be8ec3a1ca69efdb0043aff19b9d
Herminio Henriques <herminiohenriques@gmail.com>
2023-04-06 11:58:04 +0000

    Hero created

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./styles.css";
 
 // components
 import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
 
 export default function App() {
   return (
     <div className="App">
       <NavBar />
+      <Hero />
     </div>
   );
 }
