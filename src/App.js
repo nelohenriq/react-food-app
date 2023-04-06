c94f6021129239b82ad1438524b7317f35fa4888
Herminio Henriques <herminiohenriques@gmail.com>
2023-04-06 13:58:24 +0000

    Final commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import HeadlineCards from "./components/HeadlineCards";
 import Food from "./components/Food";
+import Category from "./components/Category";
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
       <Hero />
       <HeadlineCards />
       <Food />
+      <Category />
     </div>
   );
 }
