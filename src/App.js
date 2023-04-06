5668997ff7689bacc7a86f516fdfd76facfa3af8
Herminio Henriques <herminiohenriques@gmail.com>
2023-04-06 12:29:09 +0000

    HeadlineCards component added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./styles.css";
 // components
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
+import HeadlineCards from "./components/HeadlineCards";
 
 export default function App() {
   return (
     <div className="App">
       <NavBar />
       <Hero />
+      <HeadlineCards />
     </div>
   );
 }
