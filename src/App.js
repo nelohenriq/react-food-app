03b22dea9fb73a9991e6a9ce6465bfb4e33fe86d
Herminio Henriques <herminiohenriques@gmail.com>
2023-04-06 13:39:13 +0000

    Food component created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./styles.css";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import HeadlineCards from "./components/HeadlineCards";
+import Food from "./components/Food";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <NavBar />
       <Hero />
       <HeadlineCards />
+      <Food />
     </div>
   );
 }
